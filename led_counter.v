8269bbebe299b1fe991ea52c6dca88d7caad8899
Matheus de Alencar <sletezes@gmail.com>
2025-06-05 10:00:07 -0300

    tentativa 1

--- led_counter.v
+++ led_counter.v
@@ -5,6 +5,26 @@ module Counter #(
     input  wire rst_n,
     output wire [7:0] leds
 );
+    //
+    localparam CLK = CLK_FREQ - 1;
 
+    reg [31:0] counter;
+    reg [7:0] _leds;
+
+    always @(posedge clk or negedge clk) begin
+        if (!rst_n) begin
+            counter  = 32'b0;
+            _leds    = 8'b0;
+        end else begin
+            if (counter >= CLK) begin
+                counter <= 32'b0;
+                _leds   <= _leds+1;
+            end else begin
+                counter <= counter+1;
+            end
+        end
+    end
+
+    assign leds = _leds;
 
 endmodule
